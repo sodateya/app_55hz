c8c327a1ee0181ecd6c75c19f622d8632ffca892
sodateya <bssuketto@yahoo.co.jp>
2024-02-15 15:52:29 +0900

    refactor:refactoringのため削除

--- lib/version1/domain/config.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class Config {
-  Config(DocumentSnapshot doc) {
-    config = doc['ios_force_app_version'];
-  }
-  String? config;
-}
--- lib/version1/domain/user.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class User {
-  User(DocumentSnapshot doc) {
-    userID = doc['uid'];
-  }
-  String? userID;
-}
