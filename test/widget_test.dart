b49ddaa7a143196c5e574c237468e2c3b6ec6bf7
sodateya <bssuketto@yahoo.co.jp>
2024-03-28 12:34:42 +0900

    chore:rebuild

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,18 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:app_55hz/main/admob.dart';
+import 'package:app_55hz/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:app_55hz/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(
-      adInterstitial: AdInterstitial(),
-    ));
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
